8bc059ba7dbc8451cf21c99e344b3e3634701b99
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-10-28 17:10:20 +0300

    Changes for port listening

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -65,4 +69,7 @@ io.on("connection", (socket) => {
   });
 });
 
-export default app;
+// Listen
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
