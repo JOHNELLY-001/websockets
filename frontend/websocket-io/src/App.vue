acf192d3505c1e1682e388d86f94d1eb312d4d22
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-11-24 21:16:59 +0300

    Adde the online user list

--- frontend/websocket-io/src/App.vue
+++ frontend/websocket-io/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import Websocket from './components/websocket.vue';
-// import WebSocket2 from './components/webSocket2.vue';
+
 </script>
 
 <template>
   <NavBar/>
   <Websocket/>
-  <!-- <WebSocket2/> -->
 </template>
 
 <style scoped>
