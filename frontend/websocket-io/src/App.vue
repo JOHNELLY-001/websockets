fd57894173bb22315827b4c4f3c4f0cdc7ccf7bf
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-11-24 13:34:36 +0300

    Added nav bar

--- frontend/websocket-io/src/App.vue
+++ frontend/websocket-io/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
 import Websocket from './components/websocket.vue';
+import WebSocket2 from './components/webSocket2.vue';
 </script>
 
 <template>
+  <NavBar/>
   <Websocket/>
+  <!-- <WebSocket2/> -->
 </template>
 
 <style scoped>
