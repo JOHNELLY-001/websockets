9c2cdcbc7a7c1714dff0f1373540f8fa5ee8ce7f
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-11-24 13:45:22 +0300

    Added the navbar component

---
+++ frontend/websocket-io/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center justify-between py-1 bg-gray-100 rounded-t-2xl">
+    <!-- logo -->
+     <div class="ml-2">
+      <img src="../assets/horizon.jpg" alt="horizon" class="h-8 w-8 rounded-2xl">
+     </div>
+     <div class="font-bold italic text-xs mr-2 text-yellow-600">
+      <p>pop-chat</p>
+     </div>
+  </div>
+  <hr>
+</template>
